48d6d7b1874bb25ee09c594c282c6488703aa316
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-06-25 11:12:45 -0700

    This commit will provide base implimentation for the back end, setting up models and implimenting all dependencies

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,1 +1,16 @@
-const express = require('express');
+
+const express = require('express')
+const app = express()
+const PORT = 3000; 
+//const projectRoutes = require ('./routes/projectRoutes');
+const { PrismaClient } = require('./generated/prisma')
+const prisma = new PrismaClient;
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!')
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
