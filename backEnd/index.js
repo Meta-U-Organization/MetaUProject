f08115ac6dca698cf8156aa2306f8ffa28342507
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-06-25 14:20:59 -0700

    This commit will solidify CRUD for users and change password to passwordHash. It will also delete projectRoutes file in exchange for userCRUD file

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const PORT = 3000; 
-const projectRoutes = require ('./routes/projectRoutes');
+const projectRoutes = require ('./routes/userCRUD');
 app.use(express.json());
 app.use(projectRoutes);
 
