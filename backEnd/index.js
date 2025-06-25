9b4af8f1763927b6c9e21f4b98a4b653e469cfe1
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-06-25 09:42:13 -0700

    this commit will reset my backend and download an express server

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,18 +1,1 @@
-
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const PORT = 3000; 
-const projectRoutes = require ('./routes/projectRoutes');
-app.use(express.json());
-app.use(cors());
-
-app.use(projectRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Welcome to my app!')
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
+const express = require('express');
