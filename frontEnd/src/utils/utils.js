1c910308220d8aeda41320c03589a6421070aeb1
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-07-08 23:42:24 -0700

    udpates the null check in the custom hook

--- frontEnd/src/utils/utils.js
+++ frontEnd/src/utils/utils.js
@@ -5,20 +5,13 @@ export default function useCCFetch () {
     const [data, setData] = useState(null);
     const[errorType, setErrorType] = useState(null);
     const[errorMsg, setErrorMsg] = useState(null);
-    const defaultData = {
-        title: "",
-        description: "",
-        photo: "",
-        useState: "",
-        userId: 0
-    };
     const fetchData = async(url, method, body) => {
         setLoading(true);
         const backendCall = await fetch(url, {
             credentials: "include",
             method: method,
             headers: { "Content-Type": "application/json" },
-            body: body==null ?  JSON.stringify(defaultData) : body,
+            ...(body!==null ? {body: body}: {})
         });
         const newData = await backendCall.json();
         if(backendCall.status!==200){
