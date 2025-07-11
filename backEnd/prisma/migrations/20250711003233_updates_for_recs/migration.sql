290f8495d0f775adba32e93dcd8dcfba5658705c
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-07-10 17:38:35 -0700

    Updates backend schema

---
+++ backEnd/prisma/migrations/20250711003233_updates_for_recs/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "user" ADD COLUMN     "donationsReceived" INTEGER NOT NULL DEFAULT 0,
+ADD COLUMN     "lastDonationReceived" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "numTimesDonated" INTEGER NOT NULL DEFAULT 0;
