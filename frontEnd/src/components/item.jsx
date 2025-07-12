8aca8c51fe2d1e59c6804816395d417519c2509e
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-07-11 22:43:28 -0700

    update want score

--- frontEnd/src/components/item.jsx
+++ frontEnd/src/components/item.jsx
@@ -93,16 +93,11 @@ function Item({ postType, userId, isMyPost, item, onPostChange }) {
                   Want Score:{" "}
                 </label>
                 <select name="wantScore">
-                  <option value="1">1</option>
+                  <option value="1">1 - Would be cool to have</option>
                   <option value="2">2</option>
                   <option value="3">3</option>
                   <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
+                  <option value="5">5 - I NEED IT!!!!</option>
                 </select>
                 <button style={{ marginTop: "10px", marginBottom: "10px" }}>
                   Request Item
