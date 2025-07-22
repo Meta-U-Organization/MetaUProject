0cd602aa66df644f315a5148c38537f2fd0ced2e
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-07-22 09:01:55 -0700

    Updates backend to use a map instead of an array

--- backEnd/classes/WebSocketManger.js
+++ backEnd/classes/WebSocketManger.js
@@ -2,36 +2,22 @@
 class WebSocketManager {
 
     constructor() {
-        let onlineUsers = []
-        this.onlineUsers = onlineUsers;
+        this.onlineUsers = {};
     }
 
     addNewUser(userId, socketId) {
-        let i = 0;
-        while (i < this.onlineUsers.length) {
-            if (this.onlineUsers[i].userId === userId) {
-                this.onlineUsers[i].socketId = socketId;
-                break;
-            }
-            i++;
-        }
-        if (i === this.onlineUsers.length) {
-            this.onlineUsers.push({ userId, socketId });
-        }
+        this.onlineUsers[userId] = socketId;
     }
 
     deleteUser(userId) {
-        const index = this.onlineUsers.findIndex(entry => entry.userId === userId);
-
-        if (index !== -1) {
-            this.onlineUsers.splice(index, 1);
+        if (userId in this.onlineUsers) {
+            delete this.onlineUsers[userId];
         }
     }
 
-    requestNotification(userId, io, title) {
-        const entry = this.onlineUsers.filter(entry => entry.userId === userId);
-        if (entry[0].socketId !== null) {
-            io.to(entry[0].socketId).emit("getNotification")
+    requestNotification(userId, io) {
+        if (userId in this.onlineUsers) {
+            io.to(this.onlineUsers[userId]).emit("getNotification")
         }
     }
 }
