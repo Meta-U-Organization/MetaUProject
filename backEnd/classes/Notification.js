b2b2306c2057a42c2ea39e746bb86d48a83468c1
Mahesh Andres Bachu <maheshbachu@hotmail.com>
2025-07-24 09:29:48 -0700

    deletes notification object

--- backEnd/classes/Notification.js
+++
@@ -1,9 +0,0 @@
-class Notification {
-    constructor(type, description, timeCreated) {
-        this.type = type;
-        this.description = description;
-        this.timeCreated = timeCreated
-    }
-}
-
-module.exports = Notification;
